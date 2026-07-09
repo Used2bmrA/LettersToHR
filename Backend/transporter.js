f1a207bf4faf0ea5e943284827d9e3033b663129
Used2bmrA <147878998+Used2bmrA@users.noreply.github.com>
2026-07-09 15:04:29 -0600

    fix transporter

--- Backend/transporter.js
+++ Backend/transporter.js
@@ -1,9 +1,20 @@
 import nodemailer from "nodemailer";
 
-export const transporter = nodemailer.createTransport({
-    service: "gmail",
+const transporter = nodemailer.createTransport({
+
+    host: "smtp.gmail.com",
+
+    port: 465,
+
+    secure: true,
+
+    family: 4,
+
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+        pass: process.env.EMAIL_PASS
+    }
+
 });
+
+export default transporter;
