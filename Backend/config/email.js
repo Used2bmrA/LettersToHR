f51a71bd2c460833c5314b9dfd9731391fb95f4c
Used2bmrA <147878998+Used2bmrA@users.noreply.github.com>
2026-07-09 15:30:34 -0600

    y si si esta vez

--- Backend/config/email.js
+++ Backend/config/email.js
@@ -1,35 +1,5 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
-});
-
-transporter.verify((error, success)=>{
-
-    if(error){
-
-        console.log("SMTP ERROR:");
-        console.log(error);
-
-    }else{
-
-        console.log("SMTP SERVER READY");
-
-    }
-
-});
-
-
-export default transporter;
+export default resend;
